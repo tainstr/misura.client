OperationMisuraImport is now aware of time sequence units. Moved percentile_convert and unit_conversion->convert into units.py module. FIxed tentatives to interpolate empty columns in OperationMisuraImport. fixes #678
Added FileList to widgets mapping to aChooser. Override aChooser.enterEvent so it redraws if it represents a FileList.
fixes #615

misura/client/filedata/operation.py
@@ -22,7 +22,7 @@ from .. import iutils, live
 from .. import clientconf
 
 from misura.canon.csutil import profile 
-from ..plugin import units_conversion
+from .. import units
 
 from PyQt4 import QtCore
 
@@ -120,6 +120,9 @@ def interpolated(proxy,col,ztime_sequence):
 	if tdata is False:
 		return False
 	t,val=tdata[0],tdata[1]
+	if val is False or len(val)==0:
+		print 'INTERPOLATING EMPTY COLUMN', col
+		return val
 	f=InterpolatedUnivariateSpline(t,val,k=1)
 	r=f(ztime_sequence)
 	return r
@@ -203,7 +206,7 @@ class OperationMisuraImport(QtCore.QObject,base.OperationDataImportBase):
 			self.proxy=getFileProxy(self.filename)
 		else:
 			print 'COPY FILE PROXY'
- 			#self.proxy=fp.copy()
+			#self.proxy=fp.copy()
 			self.proxy=fp
 		self.job(1,'Reading file','Configuration')
 		if not self.proxy.isopen():
@@ -292,7 +295,12 @@ class OperationMisuraImport(QtCore.QObject,base.OperationDataImportBase):
 		interpolating=True
 		if doc.data.has_key(self.prefix+'t'):
 			print 'Document already have a time sequence for this prefix',self.prefix
-			time_sequence=doc.data[self.prefix+'t'].data
+			ds=doc.data[self.prefix+'t']
+			try:
+				ds=units.convert(ds, 'second')
+			except:
+				pass
+			time_sequence=ds.data
 		if len(time_sequence)==0:
 			time_sequence=np.linspace(0,elapsed-1,elapsed)
 		else:
@@ -380,7 +388,7 @@ class OperationMisuraImport(QtCore.QObject,base.OperationDataImportBase):
 				nu=self.rule_unit(col)
 				if u and nu:
 					print 'Converting to unit',col,ds.unit,nu
-					ds=units_conversion(ds,nu[0])
+					ds=convert(ds,nu[0])
 			
 			# Find out the sample index to which this dataset refers
 			var, idx=iutils.namingConvention(col)

misura/client/plugin/UnitsConverterTool.py
@@ -2,46 +2,10 @@
 # -*- coding: utf-8 -*-
 """Changes measurement unit to a dataset."""
 import veusz.plugins as plugins
-import numpy as np
-from copy import copy
 import veusz.document as document
 import utils
 from .. import units
 from misura.client.iutils import get_plotted_tree
-from PercentilePlugin import percentile_conversion
-
-def units_conversion(ds,to_unit):
-	from_unit=getattr(ds, 'unit',False)
-	if not from_unit or to_unit in ['None','',None,False]:
-		raise plugins.DatasetPluginException('Selected dataset does not have a measurement unit.') 
-	# Implicit To-From percentile conversion 
-	from_group=units.known_units[from_unit]
-	to_group=units.known_units[to_unit]
-	if from_group!=to_group:
-		if 'part' not in (from_group,to_group):
-			raise plugins.DatasetPluginException('Incompatible conversion: from {} to {}'.format(from_unit,to_unit))
-		ds1=percentile_conversion(ds)
-		if to_group=='part':
-			from_unit='percent'
-		elif from_group=='part':
-			# Guess default unit for destination dimension
-			from_unit=getattr(ds,'old_unit',units.user_defaults[to_group])
-	else:
-		# No implicit percentile conversion
-		ds1=copy(ds)
-			
-	out=units.Converter.convert(from_unit,to_unit,np.array(ds1.data))
-	ini=getattr(ds,'m_initialDimension',0)
-	old_unit=getattr(ds,'old_unit',from_unit)
-	old_group=units.known_units[old_unit]
-	if ini and (old_group==to_group==from_group) and 'part'!=to_group:
-		ini1=units.Converter.convert(from_unit,to_unit,ini)
-		ds.m_initialDimension=ini1
-		ds1.m_initialDimension=ini1
-		print 'converting m_initialDimension',ini,ini1
-	ds1.data=plugins.numpyCopyOrNone(out)
-	ds1.unit=to_unit
-	return ds1
 
 class UnitsConverterTool(utils.OperationWrapper,plugins.ToolsPlugin):
 	"""Convert between measurement units"""
@@ -78,7 +42,7 @@ class UnitsConverterTool(utils.OperationWrapper,plugins.ToolsPlugin):
 		if not ds:
 			raise plugins.DatasetPluginException('Dataset not found'+fields['ds'])
 
-		ds1=units_conversion(ds,fields['convert'])
+		ds1=units.convert(ds,fields['convert'])
 		self.ops.append(document.OperationDatasetSet(fields['ds'],ds1))
 		self.apply_ops()
 		

misura/client/units.py
@@ -104,7 +104,6 @@ def get_unit_info(unit, units):
 
 
 class Converter(object):
-	
 	from_server=lambda val:val
 	'''Convert `val` from server-side unit into client-side unit'''
 	to_client=from_server
@@ -180,4 +179,75 @@ class Converter(object):
 		"""Derivative for server->client conversion"""
 		return self.d
 
+import veusz.plugins as plugins
+from copy import copy
+import numpy as np
+def convert(ds,to_unit):
+	"""Convert dataset `ds` to `to_unit`"""
+	from_unit=getattr(ds, 'unit',False)
+	if not from_unit or to_unit in ['None','',None,False]:
+		raise plugins.DatasetPluginException('Selected dataset does not have a measurement unit.') 
+	# Implicit To-From percentile conversion 
+	from_group=known_units[from_unit]
+	to_group=known_units[to_unit]
+	if from_group!=to_group:
+		if 'part' not in (from_group,to_group):
+			raise plugins.DatasetPluginException('Incompatible conversion: from {} to {}'.format(from_unit,to_unit))
+		ds1=percentile_conversion(ds)
+		if to_group=='part':
+			from_unit='percent'
+		elif from_group=='part':
+			# Guess default unit for destination dimension
+			from_unit=getattr(ds,'old_unit',user_defaults[to_group])
+	else:
+		# No implicit percentile conversion
+		ds1=copy(ds)
+			
+	out=Converter.convert(from_unit,to_unit,np.array(ds1.data))
+	ini=getattr(ds,'m_initialDimension',0)
+	old_unit=getattr(ds,'old_unit',from_unit)
+	old_group=known_units[old_unit]
+	if ini and (old_group==to_group==from_group) and 'part'!=to_group:
+		ini1=Converter.convert(from_unit,to_unit,ini)
+		ds.m_initialDimension=ini1
+		ds1.m_initialDimension=ini1
+		print 'converting m_initialDimension',ini,ini1
+	ds1.data=plugins.numpyCopyOrNone(out)
+	ds1.unit=to_unit
+	return ds1
+	
+def percentile_conversion(ds,action='Invert',auto=True):
+	cur=getattr(ds, 'm_percent',False)
+	# invert action
+	if action=='Invert':
+		if cur: action='To Absolute'
+		else: action='To Percent'
+		print 'percentile_conversion doing',action,cur
 		
+	ini=getattr(ds, 'm_initialDimension',False)
+	out=np.array(ds.data)
+	# Auto initial dimension
+	if not ini:
+		if not auto or action!='To Percent':
+			raise plugins.DatasetPluginException('Selected dataset does not have an initial dimension set. \
+		Please first run "Initial dimension..." tool.')
+		ds.m_initialDimension=out[:5].mean()
+		
+	# Evaluate if the conversion is needed 
+	# based on the current status and the action requested by the user
+	if action=='To Absolute':
+		out=out*ds.m_initialDimension/100.
+		ds.m_percent=False
+		u=getattr(ds,'unit','percent')
+		# If current dataset unit is not percent, convert to
+		out=Converter.convert(u,'percent',out)	
+		ds.unit=getattr(ds,'old_unit',False)
+		ds.old_unit=u
+	elif action=='To Percent':
+		out=100.*out/ds.m_initialDimension
+		ds.m_percent=True
+		ds.old_unit=ds.unit
+		ds.unit='percent'
+	ds1=copy(ds)
+	ds1.data=plugins.numpyCopyOrNone(out)
+	return ds1