75254c1c1d021add0f3e36b823200fa0bb679e69
Riccardo Marotti <riccardo.marotti@gmail.com>
2015-06-11 10:18:39 +0200

    changed all prints to logging.debug ref #313

--- misura/client/bin/acquisition.py
+++ misura/client/bin/acquisition.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import logging
 from misura.client import acquisition, from_argv, iutils
 
 
 if __name__=='__main__':
 	iutils.initApp()
 	o=iutils.getOpts()
-	print 'Passed options', o
+	logging.debug('%s %s', 'Passed options', o)
 	app=iutils.app
 	mw=acquisition.MainWindow()
 	if o['-h']:
--- misura/client/bin/conf.py
+++ misura/client/bin/conf.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python 
 # -*- coding: utf-8 -*-
 import sys
+import logging
 from misura.client import iutils, conf
 from misura.client.clientconf import confdb
 
 
 
 if __name__=='__main__':
-	print 'initApp'
+	logging.debug('%s', 'initApp')
 	iutils.initApp()
-	print 'done initApp',confdb.recent_server
+	logging.debug('%s %s', 'done initApp', confdb.recent_server)
 	o=iutils.getOpts()
-	print 'Passed options', o
+	logging.debug('%s %s', 'Passed options', o)
 	fp=o['-o']
 	mc=conf.MConf(fixed_path=fp)
 	if o['-h']:
