0bc076cbc7b5d1a394dfea9d64738aeac213f778
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-19 09:40:13 +0200

    fixed test_thermal_cycle

--- misura/client/graphics/tests/test_thermal_cycle.py
+++ misura/client/graphics/tests/test_thermal_cycle.py
@@ -2,26 +2,14 @@
 # -*- coding: utf-8 -*-
 """Test for thermal cycle editor"""
 import unittest
-from misura.canon.logger import Log as logging
-from misura import utils_testing as ut
+from misura.client.tests import iutils_testing
 from misura.client.graphics import thermal_cycle
 from misura import kiln
 from PyQt4 import QtGui,QtCore
-from PyQt4.QtTest import QTest
 
-logging.debug('%s %s', 'Importing', __name__)
-
-def setUpModule():
-	logging.debug('%s %s', 'setUpModule', __name__)
-	ut.parallel(0)
-
-def tearDownModule():
-	logging.debug('%s', 'Quitting app')
-	logging.debug('%s %s', 'tearDownModule', __name__)
-	
 	
 class Designer(unittest.TestCase):
-	
+	@unittest.skipIf(__name__ != '__main__', "should be executed only manually")
 	def test(self):
 		k=kiln.Kiln()
 		k._writeLevel=5
@@ -29,12 +17,9 @@ class Designer(unittest.TestCase):
 		call=lambda f, *ar,**kw: getattr(k, f)(*ar, **kw)
 		setattr(k, 'call', call)
 		tcd=thermal_cycle.ThermalCycleDesigner(k)
-		if __name__=='__main__':
-			tcd.show()
-			QtGui.qApp.exec_()
+		tcd.show()
+		QtGui.qApp.exec_()
 		
 		
 if __name__ == "__main__":
-	unittest.main(verbosity=2)  
-	
-	
+	unittest.main(verbosity=2)
