531ae2c108d81aafebbdfe6eed93ade525bd88b9
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-23 09:50:22 +0200

    fixed test_role

--- misura/client/widgets/tests/test_role.py
+++ misura/client/widgets/tests/test_role.py
@@ -2,28 +2,16 @@
 # -*- coding: utf-8 -*-
 """Tests role selector widget."""
 import unittest
-from misura.canon.logger import Log as logging
+from misura.client.tests import iutils_testing
 from misura.client import widgets
 from misura.canon import option
 from PyQt4 import QtGui
 
-logging.debug('%s %s', 'Importing', __name__)
-main=__name__=='__main__'
-
-#TODO: generalize a widget testing  framework
-
-def setUpModule():
-	logging.debug('%s %s', 'setUpModule', __name__)
-
-def tearDownModule():
-	logging.debug('%s', 'Quitting app')
-	logging.debug('%s %s', 'tearDownModule', __name__)
-	
 	
 class Role(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
-		cls.root=option.ConfigurationProxy({
+		cls.root = option.ConfigurationProxy({
 										'self':{'role':{'current':['/dev/','default'],'type':'Role'},
 												'roleio':{'options':['/dev/','default','value'],'type':'RoleIO'}
 												},
@@ -44,10 +32,8 @@ class Role(unittest.TestCase):
 		self.assertEqual(e.tree.current_fullpath(), '/dev/')
 		self.assertEqual(e.config.itemData(e.config.currentIndex()), 'default')
 		self.assertFalse(hasattr(e, 'io'))
-		if main and False:
-			w.show()
-			QtGui.qApp.exec_()
-			logging.debug('%s %s', 'Selected on exit:', w.current)
+		
+		iutils_testing.show(w, __name__)
 					
 	def test_io(self):
 		w=widgets.build(self.root, self.root, self.root.gete('roleio'))
@@ -61,12 +47,9 @@ class Role(unittest.TestCase):
 		e.io.setCurrentIndex(ei-1)
 		nval=e.io.itemData(e.io.currentIndex())
 		e.apply()
-# 		self.assertEqual(w.prop['options'][-1], nval)
-		if main:
-			w.show()
-			QtGui.qApp.exec_()
-			logging.debug('%s %s', 'Selected on exit:', w.current)
-		
+
+		iutils_testing.show(w, __name__)
+			
 		
 		
 if __name__ == "__main__":
