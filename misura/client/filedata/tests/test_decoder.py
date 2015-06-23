d1f772e9234782bcd1c9bbb8d4d553afac3dcd64
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-23 12:17:49 +0200

    fixed test_decoder

--- misura/client/filedata/tests/test_decoder.py
+++ misura/client/filedata/tests/test_decoder.py
@@ -3,41 +3,43 @@
 """Tests DataDecoder"""
 import unittest
 
-from misura.canon.logger import Log as logging
 import os
-from misura.client.tests import iutils_testing as iut
+from misura.client.tests import iutils_testing 
 from misura.client import archive, filedata, conf
 
-nativem4=os.path.join(iut.data_dir,'hsm_test.h5')
 from PyQt4 import QtGui
 
-logging.debug('%s %s', 'Importing', __name__)
+temp_file = os.path.join(iutils_testing.data_dir, 'delete_me')
 
-def setUpModule():
-	logging.debug('%s %s', 'setUpModule', __name__)
-
-def tearDownModule():
-	logging.debug('%s %s', 'tearDownModule', __name__)
-	
-#@unittest.skip('')
 class DataDecoder(unittest.TestCase):
-	"""Tests the MainWindow"""	
-#	@unittest.skip('')
+	def tearDown(self):
+		iutils_testing.silent_remove(temp_file)
+
 	def test(self):
-		fp=filedata.getFileProxy(nativem4)
-		dec=filedata.DataDecoder()
-		dec.reset(fp,'/hsm/sample0/frame')
-		self.assertEqual(dec.ext, 'Image')
-		r=dec.get_data(0)
-		self.assertTrue(os.path.exists('%s/0.dat' % dec.tmpdir))
-		dec.reset(fp,'/hsm/sample0/profile')
-		self.assertEqual(dec.ext, 'Profile')
-		r=dec.get_data(0)
-		r[1].save('datadec0.jpg' ,'JPG')
-		r=dec.get_data(1)
-		r[1].save('datadec1.jpg','JPG')
-		r=dec.get_data(2)
-		r[1].save('datadec2.jpg','JPG')	
+		nativem4 = os.path.join(iutils_testing.data_dir, 'test_video.h5')
+		fp = filedata.getFileProxy(nativem4)
+
+		decoder = filedata.DataDecoder()
+		decoder.reset(fp,'/hsm/sample0/profile')
+		
+		self.assertEqual(decoder.ext, 'Profile')
+		
+		self.assertFalse(os.path.exists('%s/0.npy' % decoder.tmpdir))
+		r = decoder.get_data(0)
+		self.assertTrue(os.path.exists('%s/0.npy' % decoder.tmpdir))
+
+		self.assertFalse(os.path.exists(temp_file))
+		r[1].save(temp_file ,'JPG')
+		self.assertTrue(os.path.exists(temp_file))
+
+		self.assertFalse(os.path.exists('%s/1.npy' % decoder.tmpdir))
+		r = decoder.get_data(1)
+		self.assertTrue(os.path.exists('%s/1.npy' % decoder.tmpdir))
+
+		self.assertFalse(os.path.exists('%s/2.npy' % decoder.tmpdir))
+		r = decoder.get_data(2)
+		self.assertTrue(os.path.exists('%s/2.npy' % decoder.tmpdir))
+
 		fp.close()	
 
 if __name__ == "__main__":
