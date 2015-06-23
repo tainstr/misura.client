4f32c854a20fb69cb5565a05943c9e448a18d75c
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-23 09:09:46 +0200

    fixed test_aProgress

--- misura/client/widgets/tests/test_aProgress.py
+++ misura/client/widgets/tests/test_aProgress.py
@@ -3,64 +3,57 @@
 """Tests role selector widget."""
 import unittest
 import functools
-from misura.canon.logger import Log as logging
-from misura import utils_testing as ut
+from misura.client.tests import iutils_testing
 from misura.client import widgets
-from misura.device import Node 
 from misura.canon import option
 from PyQt4 import QtGui,QtCore
 from PyQt4.QtTest import QTest
 
-logging.debug('%s %s', 'Importing', __name__)
-main=__name__=='__main__'
+main = __name__=='__main__'
 
 #TODO: generalize a widget testing  framework
 
-def setUpModule():
-	logging.debug('%s %s', 'setUpModule', __name__)
-	ut.parallel(0)
-
-def tearDownModule():
-	logging.debug('%s', 'Quitting app')
-	logging.debug('%s %s', 'tearDownModule', __name__)
-	
 class aProgress(unittest.TestCase):
 	def setUp(self):
-		self.root=Node()
+		self.root = option.ConfigurationProxy()
 		self.root.sete('name', option.ao({}, 'name', 'String', 'object name')['name'])
 		
-	def wgGen(self, name='Test'):
+	def wgGen(self, name):
 		self.assertTrue(self.root.has_key(name))
-		w=widgets.build(self.root, self.root, self.root.gete(name))
-		# The current value is not initialized (gete() returns {current:None} )
-		self.assertTrue(w is not False)
-		return w
+
+		widget = widgets.build(self.root, self.root, self.root.gete(name))
+		self.assertTrue(widget is not False)
+		return widget
 		
 	def test_zero(self):
 		self.root.sete('Test', option.ao({}, 'Test', 'Progress')['Test'])
-		w=self.wgGen()
-		self.assertEqual(w.current, 0)
+		widget = self.wgGen('Test')
+		self.assertEqual(widget.current, 0)
 		if __name__=='__main__':
-			w.show()
+			widget.show()
 			QtGui.qApp.exec_()
 	
 	def test_more(self):
-		self.root.sete('Test', option.ao({}, 'Test', 'Progress', current=3, maximum=10)['Test'])
-		w=self.wgGen()
+		self.root.sete('Test', option.ao({}, 'Test', 'Progress', current = 3, max = 10)['Test'])
+		widget = self.wgGen('Test')
 		if __name__=='__main__':
-			w.show()
+			widget.show()
 			QtGui.qApp.exec_()
 			
 	def test_RoleProgress(self):
-		self.root.sete('Test', option.ao({}, 'Test', 'Progress', current=3, maximum=10)['Test'])
-		self.root.sete('Test2', option.ao({}, 'Test2', 'Progress', current=5, maximum=8)['Test2'])
+		self.root.sete('Test', option.ao({}, 'Test', 'Progress', current=3, max = 10)['Test'])
+		self.root.sete('Test2', option.ao({}, 'Test2', 'Progress', current=5, max = 8)['Test2'])
+
 		self.root.sete('progress', option.ao({}, 'progress', 'List', current=['/Test', '/Test2'])['progress'])
-		w=self.wgGen('progress')
+		self.root.setattr('progress', 'kid', '/progress')
+
+		widget = self.wgGen('progress')
 		if __name__=='__main__':
-			w.show()
+			widget.show()
 			QtGui.qApp.exec_()
 			
 if __name__ == "__main__":
 	unittest.main(verbosity=2)  
 	
 	
+
