a41612625b637d13317f2fd881d4ddee2d9fc24a
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-23 10:18:06 +0200

    fixed test_constructor

--- misura/client/conf/tests/test_constructor.py
+++ misura/client/conf/tests/test_constructor.py
@@ -3,84 +3,54 @@
 """Testing the programmatic widget construction."""
 import unittest
 import os
-from misura.canon.logger import Log as logging
 from misura.client.conf import constructor
 from misura.client import filedata
 from misura.client.misura3 import convert
-from misura.client.tests import iutils_testing as iut
+from misura.client.tests import iutils_testing
 from misura.canon import option
 from PyQt4 import QtGui,QtCore
 
-logging.debug('%s %s', 'Importing', __name__)
+nativem4 = os.path.join(iutils_testing.data_dir,'test_video.h5')
 
-def setUpModule():
-	logging.debug('%s %s', 'setUpModule', __name__)
-
-def tearDownModule():
-	logging.debug('%s %s', 'tearDownModule', __name__)
-
-main=__name__=="__main__"
-
-from3=os.path.join(iut.data_dir,'m3_hsm.h5')
-nativem4=os.path.join(iut.data_dir,'hsm_test.h5')
-
-#@unittest.skip('')
-class TestOrgSections(unittest.TestCase):
-	"""Tests categorizing options."""	
-				
-		
-#	@unittest.skip('')
+class TestConstructor(unittest.TestCase):
 	def test_orgSections(self):
-		"""Tests the distribution of scripts"""
-		fp=filedata.getFileProxy(nativem4)
-		fp.close()
-		sec=constructor.orgSections(fp.conf.post.measure.describe())
+		file_proxy = filedata.getFileProxy(nativem4)
+		file_proxy.load_conf()
+		file_proxy.close()
+
+		sec = constructor.orgSections(file_proxy.conf.post.measure.describe())
 		handles=[o['handle'] for o in sec['Main']]
 		
-		logging.debug('%s %s', '\n'.join(['%s -> %r' % (o['handle'], [c['handle'] for c in o['children']]) for o in sec['Main']]))
 		self.assertTrue('param_Sint' not in handles)
-		
-#@unittest.skip('')		
-class TestSection(unittest.TestCase):
-	"""Tests the building of a section"""
 	
-#	@unittest.skip('')
 	def test_section(self):
-		fp=filedata.getFileProxy(nativem4)
-		fp.close()
-		std=fp.conf.post.measure
+		file_proxy = filedata.getFileProxy(nativem4)
+		file_proxy.load_conf()
+		file_proxy.close()
+
+		std=file_proxy.conf.post.measure
 		sec=constructor.orgSections(std.describe())['Main']
 		
-		obj=constructor.Section(fp.conf,std,sec)
-		if main:
-			obj.show()
-			QtGui.qApp.exec_()	
-			
-#@unittest.skip('')	
-class TestInterface(unittest.TestCase):
-	"""Tests the building of a section"""
+		obj=constructor.Section(file_proxy.conf,std,sec)
+		iutils_testing.show(obj, __name__)
 	
-#	@unittest.skip('')
 	def test_interface(self):
-		fp=filedata.getFileProxy(nativem4)
-		fp.close()
-		rem=fp.conf.post.measure
-		obj=constructor.Interface(fp.conf,rem,rem.describe())
-		if main:
-			logging.debug('%s', 'Interactive testing')
-			obj.show()
-			QtGui.qApp.exec_()
+		file_proxy = filedata.getFileProxy(nativem4)
+		file_proxy.load_conf()
+		file_proxy.close()
+
+		rem = file_proxy.conf.post.measure
+		obj = constructor.Interface(file_proxy.conf,rem,rem.describe())
+		iutils_testing.show(obj, __name__)
 
 	def test_from_convert(self):
-		cp=option.ConfigurationProxy(desc=convert.tree_dict.copy())
-		cp.iterprint()
-		m=cp.hsm.measure
-		sec=constructor.orgSections(m.describe())['Main']
-		obj=constructor.Section(cp, m, sec)
-		if main:
-			logging.debug('%s', 'Interactive testing')
-			obj.show()
-			QtGui.qApp.exec_()			
+		configuration_proxy = option.ConfigurationProxy(desc=convert.tree_dict.copy())
+		configuration_proxy.iterprint()
+		measure = configuration_proxy.hsm.measure
+		sec=constructor.orgSections(measure.describe())['Main']
+		obj=constructor.Section(configuration_proxy, measure, sec)
+		
+		iutils_testing.show(obj, __name__)
 
-if main:
+if __name__ == "__main__":
 	unittest.main()  
