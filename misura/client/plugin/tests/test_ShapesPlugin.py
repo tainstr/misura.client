0b9557a06c5670c5678cafb904af0428c1e9a0f9
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-24 10:01:07 +0200

    see previous commit, but for test_ShapesPlugin

--- misura/client/plugin/tests/test_ShapesPlugin.py
+++ misura/client/plugin/tests/test_ShapesPlugin.py
@@ -3,7 +3,6 @@
 """Testing the ShapesPlugin."""
 import unittest
 import os
-from misura.canon.logger import Log as logging
 import veusz.document as document
 import veusz.plugins
 from misura.client import filedata
@@ -11,42 +10,32 @@ from misura.client.tests import iutils_testing as iut
 from misura.client import plugin
 from PyQt4 import QtGui
 
-logging.debug('%s %s', 'Importing', __name__)
-
-def setUpModule():
-	logging.debug('%s %s', 'setUpModule', __name__)
-
-def tearDownModule():
-	logging.debug('%s %s', 'tearDownModule', __name__)
-
-nativem4=os.path.join(iut.data_dir,'hsm_test.h5')
-nativem4='/home/daniele/f3x3.h5'
-
 class ShapesPlugin(unittest.TestCase):
 	"""Tests the CalibrationPlugin"""	
 
 	def do(self,doc,target):
-		fields={'sample':target,'temp':True,'time':True,'text':'$shape$\\\\%(xlabel)s=%(x)i'}
-		p=plugin.ShapesPlugin()
-		p.apply(self.cmd,fields)
+		fields={'sample': target, 'temp': True, 'time': True, 'text': '$shape$\\\\%(xlabel)s=%(x)i', 'currentwidget': '/temperature/temp'}
+		shapesPlugin = plugin.ShapesPlugin()
+		shapesPlugin.apply(self.cmd, fields)
 			
 	def test(self):
 		"""Double import a file and subtract the same datasets."""
+		nativem4 = os.path.join(iut.data_dir,'test_video.h5')
 		# Simulate an import
 		imp=filedata.OperationMisuraImport(filedata.ImportParamsMisura(filename=nativem4))
-		doc=filedata.MisuraDocument()
-		self.cmd=document.CommandInterface(doc)
-		imp.do(doc)
+		misuraDocument = filedata.MisuraDocument()
+		self.cmd = document.CommandInterface(misuraDocument)
+		imp.do(misuraDocument)
 		# Import again
-		imp.do(doc)
-		logging.debug('%s', doc.data.keys())
-		doc.model.refresh()
-		tree=doc.model.tree
-		logging.debug('%s %s', 'tree child', tree.get('').children)
-		entry=tree.traverse('hsm/sample0/h')
-		self.assertTrue(entry!=False)
-		logging.debug('%s %s', 'found entry', entry)
-		self.do(doc,entry)
+		imp.do(misuraDocument)
+		misuraDocument.model.refresh()
+		tree=misuraDocument.model.tree
+		entry=tree.traverse('0:hsm/sample0/h')
 		
+		self.assertTrue(entry!=False)
+
+		self.do(misuraDocument,entry)
+		
+
 if __name__ == "__main__":
 	unittest.main(verbosity=2)  
