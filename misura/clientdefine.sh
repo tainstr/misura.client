3d840015b0ff7a8944df874ed5b202cfc10b4ef8
Riccardo Marotti <rmarotti@tainstruments.com>
2015-06-29 16:31:00 +0200

    needed for server separation

---
+++ misura/clientdefine.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+T=`readlink -m "${BASH_SOURCE[0]}"`
+CLIENTROOT="$( cd "$( dirname "${T}" )"/.. && pwd )"
+CLIENTDIR=$CLIENTROOT/misura/client
+export PYTHONPATH=$CLIENTROOT:$PYTHONPATH
+
+####
+# Client startup functions
+####
+function mgui {
+	oldir=`pwd`
+	cd $CLIENTDIR/bin
+	python "$@"
+	cd "$oldir"
+}
+function maq {
+	mgui acquisition.py "$@"
+}
+function mcf {
+	mgui conf.py "$@"
+}
+function mgr {
+	mgui graphics.py "$@"
+}
+
+function mar {
+	mgui archive.py "$@"
+}
+
+####
+# Interactive prompt
+####
+function msc {
+	oldir=`pwd`
+	cd $CLIENTDIR
+	python -i -c "import numpy as np; \
+from cPickle import loads; \
+from misura.client import from_argv; \
+from time import sleep; \
+m=from_argv()" $@
+	cd "$oldir"
+}
