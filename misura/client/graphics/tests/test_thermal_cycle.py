217c494e905d5a67af2ea3fd51e8bbe251848db3
Riccardo Marotti <rmarotti@tainstruments.com>
2015-07-15 09:40:13 +0200

    applied pep8 to thermal cycle tests

--- misura/client/graphics/tests/test_thermal_cycle.py
+++ misura/client/graphics/tests/test_thermal_cycle.py
@@ -5,21 +5,22 @@ import unittest
 from misura.client.tests import iutils_testing
 from misura.client.graphics import thermal_cycle
 from misura import kiln
-from PyQt4 import QtGui,QtCore
+from PyQt4 import QtGui, QtCore
 
-	
+
 class Designer(unittest.TestCase):
-	@unittest.skipIf(__name__ != '__main__', "should be executed only manually")
-	def test(self):
-		k=kiln.Kiln()
-		k._writeLevel=5
-		k._readLevel=5
-		call=lambda f, *ar,**kw: getattr(k, f)(*ar, **kw)
-		setattr(k, 'call', call)
-		tcd=thermal_cycle.ThermalCycleDesigner(k)
-		tcd.show()
-		QtGui.qApp.exec_()
-		
-		
+
+    @unittest.skipIf(__name__ != '__main__', "should be executed only manually")
+    def test(self):
+        k = kiln.Kiln()
+        k._writeLevel = 5
+        k._readLevel = 5
+        call = lambda f, *ar, **kw: getattr(k, f)(*ar, **kw)
+        setattr(k, 'call', call)
+        tcd = thermal_cycle.ThermalCycleDesigner(k)
+        tcd.show()
+        QtGui.qApp.exec_()
+
+
 if __name__ == "__main__":
-	unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
--- misura/client/graphics/tests/test_thermal_cycle_row.py
+++ misura/client/graphics/tests/test_thermal_cycle_row.py
@@ -3,55 +3,67 @@
 import unittest
 from misura.client.graphics.thermal_cycle_row import ThermalCycleRow
 
+
 class TestTermalCycleRow(unittest.TestCase):
-	def setUp(self):
-		self.thermal_cycle_row = ThermalCycleRow()
 
-	def test_string_value_are_ignored(self):
-		rows = [[None, 'any string', None, None]]
+    def setUp(self):
+        self.thermal_cycle_row = ThermalCycleRow()
 
-		self.assertIs(rows[0], self.thermal_cycle_row.update_row(rows, 0, 'any mode'))
+    def test_string_value_are_ignored(self):
+        rows = [[None, 'any string', None, None]]
 
-	def test_no_numeric_values_found(self):
-		rows = [[1, 2, 3, 4], [None, 'any string', None, None], [None, 'any string', None, None]]
+        self.assertIs(
+            rows[0], self.thermal_cycle_row.update_row(rows, 0, 'any mode'))
 
-		self.assertIs(rows[0], self.thermal_cycle_row.update_row(rows, 0, 'any mode'))
+    def test_no_numeric_values_found(self):
+        rows = [[1, 2, 3, 4], [None, 'any string', None, None],
+                [None, 'any string', None, None]]
 
-	def test_rate_mode_with_0_eating_rate(self):
-		time_mode = 'ramp'
-		rows = [[0,0,0,0], [0, 100, 0, 0]]
+        self.assertIs(
+            rows[0], self.thermal_cycle_row.update_row(rows, 0, 'any mode'))
 
-		self.assertEqual([0,100,0,0], self.thermal_cycle_row.update_row(rows, 1, time_mode))
+    def test_rate_mode_with_0_eating_rate(self):
+        time_mode = 'ramp'
+        rows = [[0, 0, 0, 0], [0, 100, 0, 0]]
 
-	def test_rate_mode_with_eating_rate_set(self):
-		time_mode = 'ramp'
-		rows = [[0,0,0,0], [0, 100, 20, 0]]
-		self.assertEqual([5,100,20,5], self.thermal_cycle_row.update_row(rows, 1, time_mode))
+        self.assertEqual(
+            [0, 100, 0, 0], self.thermal_cycle_row.update_row(rows, 1, time_mode))
 
-		rows = [[0,0,0,0], [5,100,20,5], [0, 200, 0, 0]]
-		self.assertEqual([5,200, 0, 0], self.thermal_cycle_row.update_row(rows, 2, time_mode))
+    def test_rate_mode_with_eating_rate_set(self):
+        time_mode = 'ramp'
+        rows = [[0, 0, 0, 0], [0, 100, 20, 0]]
+        self.assertEqual(
+            [5, 100, 20, 5], self.thermal_cycle_row.update_row(rows, 1, time_mode))
 
-		rows = [[0,0,0,0], [5,100,20,5], [5, 200, 20, 0]]
-		self.assertEqual([10,200, 20, 5], self.thermal_cycle_row.update_row(rows, 2, time_mode))
+        rows = [[0, 0, 0, 0], [5, 100, 20, 5], [0, 200, 0, 0]]
+        self.assertEqual(
+            [5, 200, 0, 0], self.thermal_cycle_row.update_row(rows, 2, time_mode))
 
-	def test_time_mode(self):
-		time_mode = 'points'
-		rows = [[0,30,0,0], [0, 100, 0, 0]]
-		self.assertEqual([0,100,0,0], self.thermal_cycle_row.update_row(rows, 1, time_mode))
+        rows = [[0, 0, 0, 0], [5, 100, 20, 5], [5, 200, 20, 0]]
+        self.assertEqual(
+            [10, 200, 20, 5], self.thermal_cycle_row.update_row(rows, 2, time_mode))
 
-		rows = [[0,30,0,0], [10, 100, 0, 0]]
-		self.assertEqual([10,100,7,10], self.thermal_cycle_row.update_row(rows, 1, time_mode))
+    def test_time_mode(self):
+        time_mode = 'points'
+        rows = [[0, 30, 0, 0], [0, 100, 0, 0]]
+        self.assertEqual(
+            [0, 100, 0, 0], self.thermal_cycle_row.update_row(rows, 1, time_mode))
 
-	def test_duration_mode(self):
-		time_mode = 'dwell'
+        rows = [[0, 30, 0, 0], [10, 100, 0, 0]]
+        self.assertEqual(
+            [10, 100, 7, 10], self.thermal_cycle_row.update_row(rows, 1, time_mode))
 
-		rows = [[0,30,0,0], [0, 30, 0, 0]]
-		self.assertEqual([0,30,0,0], self.thermal_cycle_row.update_row(rows, 1, time_mode))
+    def test_duration_mode(self):
+        time_mode = 'dwell'
 
-		rows = [[0,30,0,0], [0, 30, 0, 10]]
-		self.assertEqual([10,30,0,10], self.thermal_cycle_row.update_row(rows, 1, time_mode))
+        rows = [[0, 30, 0, 0], [0, 30, 0, 0]]
+        self.assertEqual(
+            [0, 30, 0, 0], self.thermal_cycle_row.update_row(rows, 1, time_mode))
 
+        rows = [[0, 30, 0, 0], [0, 30, 0, 10]]
+        self.assertEqual(
+            [10, 30, 0, 10], self.thermal_cycle_row.update_row(rows, 1, time_mode))
 
 
 if __name__ == "__main__":
-	unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
