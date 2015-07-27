bb0d9a978f06a1bdf892b2a0b623c7e215a74f35
Riccardo Marotti <rmarotti@tainstruments.com>
2015-07-27 12:20:40 +0200

    implemented gaps removal

--- misura/client/plugin/RemoveGapsPlugin.py
+++ misura/client/plugin/RemoveGapsPlugin.py
@@ -6,6 +6,21 @@ import numpy
 from utils import smooth
 
 
+def remove_gaps_from(data, threshold):
+    clean_data = numpy.copy(data)
+    diffs = numpy.diff(clean_data)
+    gaps_indexes = numpy.where(numpy.abs(diffs) > threshold)[0]
+
+    for gap_index in gaps_indexes:
+        for i in range(gap_index+1, len(clean_data)):
+            clean_data[i] = clean_data[i] - diffs[gap_index]
+
+    return clean_data
+
+
+
+
+
 class RemoveGapsPlugin(plugins.DatasetPlugin):
     # tuple of strings to build position on menu
     menu = ('Filter', 'Remove Gaps')
@@ -20,18 +35,20 @@ class RemoveGapsPlugin(plugins.DatasetPlugin):
         self.fields = [
             plugins.FieldDataset('input_dataset', 'Input Dataset Name', default = input_dataset),
             plugins.FieldDataset('output_dataset', 'Output dataset name', default = output_dataset),
+            plugins.FieldInt('gap_amplitutde', 'Minimum gap amplitude', default=5),
         ]
 
     def getDatasets(self, fields):
         self.output_dataset = plugins.Dataset1D(fields['output_dataset'])
-
         return [self.output_dataset]
 
     def updateDatasets(self, fields, helper):
         input_dataset = helper.getDataset(fields['input_dataset'])
-        data = numpy.array(input_dataset.data)
-        self.output_dataset.update(data=data, serr=input_dataset.serr, perr=input_dataset.perr, nerr=input_dataset.nerr)
+        gap_amplitutde = fields['gap_amplitutde']
+        cleaned_data = remove_gaps_from(input_dataset.data, gap_amplitutde)
+        self.output_dataset.update(data=cleaned_data, serr=input_dataset.serr, perr=input_dataset.perr, nerr=input_dataset.nerr)
         return [self.output_dataset]
 
 # add plugin classes to this list to get used
 plugins.datasetpluginregistry.append(RemoveGapsPlugin)
+
