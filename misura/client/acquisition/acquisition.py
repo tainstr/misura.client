tainstr/misura.client

commit 340ea9d3c136fd58a6249ffec1cdf4733d4fdadf

added test for resetFileProxy

misura/client/acquisition/acquisition.py
@@ -79,8 +79,14 @@ class MainWindow(QtGui.QMainWindow):
     doc = False
     uid = False
 
+    def __init__(self):
+        self._tasks = None
+
     @property
     def tasks(self):
+        if self._tasks:
+            return self._tasks
+
         # DEBUG needed for UT
         if not registry.tasks:
             logging.debug('Preparing registry.tasks object')
@@ -492,7 +498,6 @@ class MainWindow(QtGui.QMainWindow):
         except:
             pass
 
-    @csutil.profile
     def _resetFileProxy(self, retry=0, recursion=0):
         """Resets acquired data widgets"""
         if self._blockResetFileProxy:
@@ -501,13 +506,11 @@ class MainWindow(QtGui.QMainWindow):
 
         if self.doc:
             self.doc.close()
-            self.release_lock()
             self.doc = False
+
         doc = False
         fid = False
-        if recursion > sys.getrecursionlimit() - 10:
-            self.release_lock()
-            return False
+
         if self.fixedDoc is not False:
             fid = 'fixedDoc'
             doc = self.fixedDoc

misura/client/acquisition/tests/test_acquisition.py
@@ -9,6 +9,10 @@ from misura.canon import option
 from misura import server
 from PyQt4 import QtGui
 
+from misura.client.live import registry
+
+class Dummy(object):
+    pass
 
 class MainWindow(unittest.TestCase):
 
@@ -19,6 +23,17 @@ class MainWindow(unittest.TestCase):
         self.instr = self.server.flex
         self.instr.init_instrument = lambda *foo: True
         self.main_window = acquisition.MainWindow()
+
+        self.release_lock_calls_count = 0
+        self.resetFileProxyLater_calls_count = 0
+        self.main_window.release_lock = lambda : self.increase_release_lock_calls()
+        self.main_window.resetFileProxyLater = lambda retry, recursion: self.increase_resetFileProxyLater_calls()
+
+    def increase_release_lock_calls(self):
+        self.release_lock_calls_count += 1
+
+    def increase_resetFileProxyLater_calls(self):
+        self.resetFileProxyLater_calls_count += 1
 
     def tearDown(self):
         self.main_window.close()
@@ -58,6 +73,26 @@ class MainWindow(unittest.TestCase):
             main_window.controls.stopAct.isEnabled(), self.server['isRunning'])
         self.assertEqual(main_window.measureTab.count(), 4)
         self.assertEqual(main_window.name, 'flex')
+
+    def test_resetFileProxyBlocked(self):
+        self.main_window._blockResetFileProxy = True
+
+        self.assertFalse(self.main_window._resetFileProxy())
+        self.assertEqual(1, self.release_lock_calls_count)
+
+    def test_resetFileProxy_during_initTest(self):
+        self.main_window.server = {'initTest': 1}
+        dummy = Dummy()
+        setattr(dummy.__class__, 'jobs', lambda x,y,z: True)
+        setattr(dummy.__class__, 'setFocus', lambda x : True)
+
+        self.main_window._tasks = dummy
+
+        self.assertFalse(self.main_window._resetFileProxy())
+        self.assertEqual(1, self.resetFileProxyLater_calls_count)
+
+
+
 
 
 if __name__ == "__main__":

commit ca1ad71707340c6753c34aa2150720c2443c4b0b

fixed problem caused by 340ea9d3c136fd58

misura/client/acquisition/acquisition.py
@@ -79,12 +79,10 @@ class MainWindow(QtGui.QMainWindow):
     doc = False
     uid = False
 
-    def __init__(self):
-        self._tasks = None
 
     @property
     def tasks(self):
-        if self._tasks:
+        if getattr(self, '_tasks', False):
             return self._tasks
 
         # DEBUG needed for UT
@@ -93,6 +91,7 @@ class MainWindow(QtGui.QMainWindow):
             registry.set_manager(network.manager)
             if self.server and not self.fixedDoc:
                 registry.progress.set_server(self.server)
+
         return registry.tasks
 
     def __init__(self, doc=False, parent=None):