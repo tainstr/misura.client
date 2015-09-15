b4951ffbb0f8cb2631ac13198f5518ce9d82f8bb
Riccardo Marotti <rmarotti@tainstruments.com>
2015-09-15 12:05:09 +0200

    Added table creation for hsm image export.
    refs 829

--- misura/client/fileui/html.py
+++ misura/client/fileui/html.py
@@ -3,3 +3,13 @@ import base64
 def embed(data, type):
 	encoded_data = base64.b64encode(data)
 	return "<img src='data:image/%s;base64,%s' alt=''>" % (type, encoded_data)
+
+def table_from(images, type='jpg', images_per_line=5):
+	html = "<table><tr>"
+
+	for index, image in enumerate(images):
+		html = html + "<td>%s</td>" % embed(image, type)
+		if (index + 1) % images_per_line == 0:
+			html = html + "</tr><tr>"
+
+	return html + "</tr></table>"
--- misura/client/fileui/tests/test_html.py
+++ misura/client/fileui/tests/test_html.py
@@ -17,6 +17,32 @@ class Html(unittest.TestCase):
 
 		self.assertEqual(expected_html, html.embed("any data", "jpg"))
 
+	def test_table_no_inages(self):
+		expected_html = "<table><tr></tr></table>"
+
+		self.assertEqual(expected_html, html.table_from([]))
+
+	def test_table_with_one_image(self):
+		image_html = "<img src='data:image/jpg;base64,YW55IGRhdGE=' alt=''>"
+		expected_html = "<table><tr><td>%s</td></tr></table>" % image_html
+
+		self.assertEqual(expected_html, html.table_from(['any data']))
+
+	def test_table_with_two_images(self):
+		image_html1 = "<img src='data:image/jpg;base64,YW55IGRhdGEgMQ==' alt=''>"
+		image_html2 = "<img src='data:image/jpg;base64,YW55IGRhdGEgMg==' alt=''>"
+
+		expected_html = "<table><tr><td>%s</td><td>%s</td></tr></table>" % (image_html1, image_html2)
+
+		self.assertEqual(expected_html, html.table_from(['any data 1', 	'any data 2']))
+
+	def test_table_with_six_images(self):
+		image_html = "<img src='data:image/jpg;base64,YW55IGRhdGE=' alt=''>"
+
+		expected_html = "<table><tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr><tr><td>%s</td></tr></table>" % (image_html, image_html, image_html, image_html, image_html, image_html)
+
+		self.assertEqual(expected_html, html.table_from(['any data', 'any data', 'any data', 'any data', 'any data', 'any data']))
+
 
 
 
