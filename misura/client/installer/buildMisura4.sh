65958d57db681bdf2aee5e6e37f358b2b3bd1e14
Riccardo Marotti <rmarotti@tainstruments.com>
2015-11-04 16:44:03 +0100

    added tbz2 file creation to linux client installer
    refs #925

--- misura/client/installer/buildMisura4.sh
+++ misura/client/installer/buildMisura4.sh
@@ -7,8 +7,15 @@ ln -s /opt/misura4/misura.canon/misura/canon /opt/misura4/misura.client/misura/c
 pyinstaller -y client_linux_pyinst.spec
 rm /opt/misura4/misura.client/misura/canon
 
-OUT_DIR=dist
+OUT_DIR=/opt/misura4/misura.client/misura/client/installer/dist
 
 cp $OUT_DIR/configuration/* $OUT_DIR/misura4/
 cp $OUT_DIR/browser/* $OUT_DIR/misura4/
 cp $OUT_DIR/acquisition/* $OUT_DIR/misura4/
+
+cd $OUT_DIR
+tar c misura4 | bzip2 -9 > misura4.tbz2
+cd -
+
+echo Done: you can find misura4 tbz2 in $OUT_DIR
+echo
