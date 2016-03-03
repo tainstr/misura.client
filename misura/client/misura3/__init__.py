aeded3f20fdda7287d5fec0add964475b04347a1
Daniele Paganelli <dpaganelli@tainstruments.com>
2016-03-03 16:56:51 +0100

    Made m3 plugin optional to remove pyodbc dependency for non-m3 developers, FLAS-19

--- misura/client/misura3/__init__.py
+++ misura/client/misura3/__init__.py
@@ -1,4 +1,10 @@
 """ """
-from convert import Converter
-from dialog import TestDialog
+# TODO: migrate to new dataimport plugin mechanism
+try:
+    from convert import Converter
+    from dialog import TestDialog
+except:
+    Converter = False
+    TestDialog = False
+
 
