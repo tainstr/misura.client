ced3daf7d7fc2d61de4fe3fb5014d8d30c6c3255
Riccardo Marotti <rmarotti@tainstruments.com>
2016-03-31 11:03:23 +0200

    Minor
    ESS-190

--- misura/client/procedure/tests/test_thermal_cycle.py
+++ misura/client/procedure/tests/test_thermal_cycle.py
@@ -44,8 +44,8 @@ class Designer(unittest.TestCase):
             'stepsDeltaT': 10,
             'firstStepTemperature': 1000
         }
-        self.assertEqual([[0.0, 0], [3000.0, 1000], [3120.0, 1000]],
-                         thermal_cycle.steps_template_to_thermal_cycle_curve(no_steps))
+        assert_equals([[0.0, 0], [3000.0, 1000], [3120.0, 1000]],
+                      thermal_cycle.steps_template_to_thermal_cycle_curve(no_steps))
 
         no_steps2 = {
             'heatingRate': 60,
@@ -54,8 +54,8 @@ class Designer(unittest.TestCase):
             'stepsDeltaT': 10,
             'stasisDuration': 120
         }
-        self.assertEqual([[0.0, 0], [1200.0, 1200], [1320.0, 1200]],
-                         thermal_cycle.steps_template_to_thermal_cycle_curve(no_steps2))
+        assert_equals([[0.0, 0], [1200.0, 1200], [1320.0, 1200]],
+                      thermal_cycle.steps_template_to_thermal_cycle_curve(no_steps2))
 
         one_step = {
             'heatingRate': 60,
@@ -64,8 +64,8 @@ class Designer(unittest.TestCase):
             'stepsDeltaT': 120,
             'stasisDuration': 100
         }
-        self.assertEqual([[0.0, 0], [1200.0, 1200], [1300.0, 1200]],
-                         thermal_cycle.steps_template_to_thermal_cycle_curve(one_step))
+        assert_equals([[0.0, 0], [1200.0, 1200], [1300.0, 1200]],
+                      thermal_cycle.steps_template_to_thermal_cycle_curve(one_step))
 
         two_steps = {
             'heatingRate': 60,
@@ -74,8 +74,8 @@ class Designer(unittest.TestCase):
             'stasisDuration': 120,
             'stepsDeltaT': 120
         }
-        self.assertEqual([[0.0, 0], [1200.0, 1200], [1320.0, 1200], [1440, 1320], [1560, 1320]],
-                         thermal_cycle.steps_template_to_thermal_cycle_curve(two_steps))
+        assert_equals([[0.0, 0], [1200.0, 1200], [1320.0, 1200], [1440, 1320], [1560, 1320]],
+                      thermal_cycle.steps_template_to_thermal_cycle_curve(two_steps))
 
 
     def test_progress_bar_position(self):
