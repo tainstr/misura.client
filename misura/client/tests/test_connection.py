ed0211b75869aa804db9f5859fbcd701bae81675
Riccardo Marotti <rmarotti@tainstruments.com>
2016-04-05 15:33:10 +0200

    Minor test on LiveLog.slotUpdate

---
+++ misura/client/tests/test_connection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""misura Configuration Manager"""
+import unittest
+from misura.client.tests import iutils_testing
+from misura.client import connection
+from misura.client.live import registry
+
+class LiveLog(unittest.TestCase):
+
+    def test_no_updates_when_log_is_unchanged(self):
+        registry.log_buf = [1,2,3,4]
+        log = connection.LiveLog()
+        log.current_buf = [1,2,3,4]
+
+        log.slotUpdate()
+
+        self.assertEquals([1,2,3,4], log.current_buf)
+
+    def test_update(self):
+        registry.log_buf = [1,2,3,4]
+        log = connection.LiveLog()
+        log.current_buf = [1,2]
+
+        log.slotUpdate()
+
+        self.assertEquals([1,2,3,4], log.current_buf)
+
+
+if __name__ == "__main__":
+    unittest.main()
