156125796071d58241ff196cb57a08e7bf552c70
Daniele Paganelli <dpaganelli@tainstruments.com>
2016-04-14 12:50:32 +0200

    DefaultPlotPlugin accepts target graphs field. FLTD-18

--- misura/client/plugin/DefaultPlotPlugin.py
+++ misura/client/plugin/DefaultPlotPlugin.py
@@ -4,7 +4,6 @@
 import re
 
 import veusz.plugins as plugins
-import veusz.document as document
 from misura.client import iutils
 from misura.canon.logger import Log as logging
 from .. import axis_selection
@@ -23,22 +22,71 @@ class DefaultPlotPlugin(plugins.ToolsPlugin):
     # text to appear in dialog box
     description_full = 'Default plot for Misura datasets'
 
-    def __init__(self, dsn=[], rule=''):
+    def __init__(self, dsn=[], rule='', graphs = ['/time/time', '/temperature/temp']):
         """Make list of fields."""
 
         self.fields = [
             plugins.FieldDatasetMulti("dsn", 'Dataset names'),
-            plugins.FieldText("rule", 'Plotting rule', default=rule)
+            plugins.FieldText("rule", 'Plotting rule', default=rule),
+            plugins.FieldTextMulti("graphs", 'Target graphs', default=graphs),
         ]
-
+        
+    def get_time_datasets(self, names, graph):
+        x = []
+        y = []
+        for ds in names:
+            prefix = ''
+            if ':' in ds:
+                prefix, ds1 = ds.split(':')
+                prefix += ':'
+            timeds = axis_selection.get_best_x_for(
+                ds, prefix, self.doc.data, graph)
+            if timeds:
+                y.append(ds)
+                x.append(timeds)
+            else:
+                print 'NO TIMEDS FOUND FOR', ds     
+        return x, y
+                
+    def get_temperature_datasets(self, names, graph):
+        x = []
+        y = []
+        for ds in names:
+            if axis_selection.is_temperature(ds):
+                continue
+            prefix = ''
+            if ':' in ds:
+                prefix, ds1 = ds.split(':')
+                prefix += ':'        
+            ds_temperature = axis_selection.get_best_x_for(
+                ds, prefix, self.doc.data, graph)
+            if ds_temperature:
+                x.append(ds_temperature)
+                y.append(ds)  
+        return x, y      
+          
+    def plot_on_graph(self, names, graph):
+        print 'plot_on_graph', graph, len(names)
+        if graph.endswith('/temp'):
+            x, y = self.get_temperature_datasets(names, graph)
+        elif graph.endswith('/time'):
+            x, y = self.get_time_datasets(names, graph)      
+        
+        result = PlotDatasetPlugin().apply(
+            self.cmd, {'x': x, 'y': y, 'currentwidget': graph})
+        
+        return result
+        
     def apply(self, cmd, fields):
         """Do the work of the plugin.
         cmd: veusz command line interface object (exporting commands)
         fields: dict mapping field names to values
         """
+        self.cmd = cmd
         self.doc = cmd.document
+        
         dsn = fields['dsn']
-        vars = []
+        names = []
         rp = fields['rule'].replace('\n', '|')
         if len(rp) > 0:
             rp = re.compile(rp)
@@ -51,36 +99,14 @@ class DefaultPlotPlugin(plugins.ToolsPlugin):
             var = iutils.namingConvention(ds1)[0]
             logging.debug('%s %s %s', 'Checking', ds, var)
             if rp and rp.search(ds1):
-                vars.append(ds)
+                names.append(ds)
 
-        vars = sorted(vars)
+        names = sorted(names)
         logging.debug('%s %s', "VARS", vars)
-        xt = []
-        xT = []
-        yt = []
-        yT = []
-        for ds in vars:
-            prefix = ''
-            if ':' in ds:
-                prefix, ds1 = ds.split(':')
-                prefix += ':'
-            timeds = axis_selection.get_best_x_for(
-                ds, prefix, self.doc.data, '/time/time')
-            if timeds:
-                yt.append(ds)
-                xt.append(timeds)
-            else:
-                print 'NO TIMEDS FOUND FOR', ds
-
-            if not axis_selection.is_temperature(ds):
-                ds_temperature = axis_selection.get_best_x_for(
-                    ds, prefix, self.doc.data, '/temperature/temp')
-                if ds_temperature:
-                    xT.append(ds_temperature)
-                    yT.append(ds)
-
-        result = PlotDatasetPlugin().apply(
-            cmd, {'x': xt, 'y': yt, 'currentwidget': '/time/time'})
-        result.update(PlotDatasetPlugin().apply(
-            cmd, {'x': xT, 'y': yT, 'currentwidget': '/temperature/temp'}))
+        
+        result={}
+        graphs = fields.get('graphs',['/time/time', '/temperature/temp'])
+        for graph in graphs:
+            r = self.plot_on_graph(names, graph)
+            result.update(r)
         return result
--- misura/client/plugin/PlotPlugin.py
+++ misura/client/plugin/PlotPlugin.py
@@ -5,14 +5,12 @@ import veusz.plugins as plugins
 import veusz.document as document
 from ArrangePlugin import ArrangePlugin
 from time import time
-from misura.client import iutils
 from misura.client.iutils import get_plotted_tree
 import os
 from misura.canon.logger import Log as logging
 import utils
 import PercentilePlugin
 from .. import units
-from .. import axis_selection
 
 default_curves = ['T', 'P', 'S', 'h', 'Vol', 'd', 'err']
 an_default_curves = ['T', 'Vol', 'd', 'Dil',  'Sint', 'Flex']
@@ -250,83 +248,6 @@ class PlotDatasetPlugin(utils.OperationWrapper, plugins.ToolsPlugin):
 
         return cnames
 
-
-import re
-
-
-class DefaultPlotPlugin(plugins.ToolsPlugin):
-
-    """Plot Misura datasets."""
-    # a tuple of strings building up menu to place plugin on
-    menu = ('Misura', 'Make a default plot from a list of datasets')
-    # unique name for plugin
-    name = 'Misura default plot'
-    # name to appear on status tool bar
-    description_short = 'Default plot for Misura datasets'
-    # text to appear in dialog box
-    description_full = 'Default plot for Misura datasets'
-
-    def __init__(self, dsn=[], rule=''):
-        """Make list of fields."""
-
-        self.fields = [
-            plugins.FieldDatasetMulti("dsn", 'Dataset names'),
-            plugins.FieldText("rule", 'Plotting rule', default=rule)
-        ]
-
-    def apply(self, cmd, fields):
-        """Do the work of the plugin.
-        cmd: veusz command line interface object (exporting commands)
-        fields: dict mapping field names to values
-        """
-        self.doc = cmd.document
-        dsn = fields['dsn']
-        vars = []
-        rp = fields['rule'].replace('\n', '|')
-        if len(rp) > 0:
-            rp = re.compile(rp)
-        else:
-            rp = False
-        for ds in dsn:
-            ds1 = ds
-            if ':' in ds1:
-                ds1 = ds1.split(':')[1]
-            var = iutils.namingConvention(ds1)[0]
-            logging.debug('%s %s %s', 'Checking', ds, var)
-            if rp and rp.search(ds1):
-                vars.append(ds)
-
-        vars = sorted(vars)
-        logging.debug('%s %s', "VARS", vars)
-        xt = []
-        xT = []
-        yt = []
-        yT = []
-        for ds in vars:
-            prefix = ''
-            if ':' in ds:
-                prefix, ds1 = ds.split(':')
-                prefix += ':'
-            timeds = axis_selection.get_best_x_for(
-                ds, prefix, self.doc.data, '/time/time')
-            if timeds:
-                yt.append(ds)
-                xt.append(timeds)
-            else:
-                print 'NO TIMEDS FOUND FOR', ds
-
-            if not axis_selection.is_temperature(ds):
-                ds_temperature = axis_selection.get_best_x_for(
-                    ds, prefix, self.doc.data, '/temperature/temp')
-                if ds_temperature:
-                    xT.append(ds_temperature)
-                    yT.append(ds)
-
-        result = PlotDatasetPlugin().apply(
-            cmd, {'x': xt, 'y': yt, 'currentwidget': '/time/time'})
-        result.update(PlotDatasetPlugin().apply(
-            cmd, {'x': xT, 'y': yT, 'currentwidget': '/temperature/temp'}))
-        return result
 
 # INTENTIONALLY NOT PUBLISHED
 # plugins.toolspluginregistry.append(PlotDatasetPlugin)
