b76b0f5d8ef5519b7e5a1097edba259c549faf13
Daniele Paganelli <dpaganelli@tainstruments.com>
2016-11-15 17:05:20 +0100

    Hide thermalCycle tab from Flash tests, FLTD-214

--- misura/client/acquisition/measureinfo.py
+++ misura/client/acquisition/measureinfo.py
@@ -35,20 +35,20 @@ class MeasureInfo(QtGui.QTabWidget):
         else:
             self.thermalCycleView = QtGui.QWidget(self)
         self.results = QtGui.QWidget()
-        
+
         self.nobj = widgets.ActiveObject(
             self.server, self.remote.measure, self.remote.measure.gete('nSamples'), parent=self)
-        
+
         self.refreshSamples()
-        
+
         self.nobj.register()
         self.connect(self.nobj, QtCore.SIGNAL('changed()'),
                      self.refreshSamples, QtCore.Qt.QueuedConnection)
         self.connect(
             self, QtCore.SIGNAL("currentChanged(int)"), self.tabChanged)
         self.connect(
-           self, QtCore.SIGNAL("currentChanged(int)"), self.refreshSamples)
-        
+            self, QtCore.SIGNAL("currentChanged(int)"), self.refreshSamples)
+
         self.tabBar().setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.tabBar().customContextMenuRequested.connect(self.show_menu)
 
@@ -67,38 +67,39 @@ class MeasureInfo(QtGui.QTabWidget):
                 self.fromthermalCycleView = False
         else:
             self.fromthermalCycleView = True
-        
 
     def checkCurve(self):
         remote_equals, saved_equals = self.thermalCycleView.check_if_saved()
         if not remote_equals:
             r = QtGui.QMessageBox.warning(self, _("Changes were not applied"),
-                                          _("Changes to thermal cycle were not applied! Apply now?"),
+                                          _(
+                                              "Changes to thermal cycle were not applied! Apply now?"),
                                           QtGui.QMessageBox.Cancel | QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
             if r == QtGui.QMessageBox.Ok:
                 self.thermalCycleView.apply()
         # TODO: warn also about saved_equals?
 
     nsmp = 0
-    
+
     def set_results(self, results):
         i = self.indexOf(self.results)
-        if i>=0:
+        if i >= 0:
             self.removeTab(i)
         else:
-            i=self.count()
+            i = self.count()
         self.results = results
         self.insertTab(i, self.results, _('Results'))
 
     def refreshSamples(self, *foo):
         print 'REFRESH SAMPLES'
         nsmp = self.nobj.current
-        if self.nsmp == nsmp and self.nodeViews and self.nodes and self.count()>2:
+        if self.nsmp == nsmp and self.nodeViews and self.nodes and self.count() > 2:
             logging.debug('NO CHANGE in samples number', self.nsmp, nsmp)
             return False
         self.nsmp = nsmp
         self.blockSignals(True)
         self.clear()
+        
         if not self.fixedDoc:
             self.statusView = status.Status(
                 self.server, self.remote, parent=self)
@@ -108,25 +109,28 @@ class MeasureInfo(QtGui.QTabWidget):
         else:
             self.statusView = False
         self.addTab(self.measureView, _('Measure'))
-        self.addTab(self.thermalCycleView, _('Thermal Cycle'))
+        if self.remote['devpath'] != 'flash':
+            self.addTab(self.thermalCycleView, _('Thermal Cycle'))
+        else:
+            self.thermalCycleView.hide()
         self.addTab(self.results, _('Results'))
         self.refresh_nodes()
         self.blockSignals(False)
         return True
-    
+
     def get_samples(self):
         nsmp = self.nobj.current
         self.nsmp = nsmp
         paths = []
         p0 = self.remote['fullpath']
         for i in range(self.nsmp):
-            n =  'sample' + str(i)
+            n = 'sample' + str(i)
             sample = getattr(self.remote, n, False)
             if sample:
                 paths.append(p0 + n)
         print 'GETSAMPLES', paths
         return paths
-    
+
     def refresh_nodes(self, nodes=[]):
         # Remove all node tabs
         for nodevi in self.nodeViews.itervalues():
@@ -134,7 +138,7 @@ class MeasureInfo(QtGui.QTabWidget):
             if i < 0:
                 continue
             self.removeTab(i)
-            
+
         if not nodes:
             nodes = self.nodes
         if not nodes:
@@ -155,20 +159,20 @@ class MeasureInfo(QtGui.QTabWidget):
             wg = self.nodeViews.get(n, False)
             if not wg:
                 wg = conf.Interface(
-                        self.server, node, node.describe(), self)
+                    self.server, node, node.describe(), self)
             j = c - 1 + i
             self.insertTab(j, wg, node['devpath'].capitalize())
             self.nodeViews[n] = wg
             self.nodes.append(n)
         return True
-    
+
     def show_menu(self, pos):
         tab_idx = self.tabBar().tabAt(pos)
         wg = self.widget(tab_idx)
         node = getattr(wg, 'remObj', False)
         if not node:
             return
-        node = '0:'+node['fullpath'][1:-1]
+        node = '0:' + node['fullpath'][1:-1]
         node = self.results.navigator.doc.model.tree.traverse(node)
         if not node:
             return
