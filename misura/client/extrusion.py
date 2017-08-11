a09383b6b5578d736eb53c496c7c64f481c5ea9f
Daniele Paganelli <dpaganelli@tainstruments.com>
2017-08-12 00:05:37 +0200

    Interactive ExtrusionRender controls and color legend

--- misura/client/extrusion.py
+++ misura/client/extrusion.py
@@ -8,7 +8,6 @@ from misura.canon.reference import get_node_reference
 from misura.canon.option import ao, ConfigurationProxy
 from misura.canon.logger import get_module_logging
 logging = get_module_logging(__name__)
-
 from misura.client import widgets, conf, _
 
 try:
@@ -20,12 +19,13 @@ except:
     gl = False
     enabled = False
     logging.debug('Disabled 3D: no pyqtgraph')
-    
+
 from PyQt4 import QtCore, QtGui
 
 STEPS = np.array([0.0, 0.33, 0.66, 1.0])
 CLRS = ['b', 'r', 'y', 'w']
 
+clrmp = False
 if pg:
     clrmp = pg.ColorMap(STEPS, np.array(
         [pg.colorTuple(pg.Color(c)) for c in CLRS]))
@@ -34,21 +34,21 @@ if pg:
 # TODO: parametrize start, end, max_layers
 # TODO: step by time/temperature
 # TODO: write temperature labels
-def read_file(profile, startTime=10, endTime=-1, maxLayers=500, cut=0, 
-                T=False,
-                aborted=[False],
-                jobs=lambda *x, **k: 0,
-                job=lambda *x, **k: 0,
-                done=lambda *x, **k: 0):
+def read_file(profile, startTime=10, endTime=-1, maxLayers=500, cut=0, deplete=1,
+              T=False,
+              aborted=[False],
+              jobs=lambda *x, **k: 0,
+              job=lambda *x, **k: 0,
+              done=lambda *x, **k: 0):
     xs = []
     ys = []
     zs = []
     colors = []
     start = 0
-    if startTime>0:
+    if startTime > 0:
         start = profile.get_time(startTime)
     end = -1
-    if endTime>0:
+    if endTime > 0:
         end = profile.get_time(endTime)
     c = 0.
     n = len(profile)
@@ -61,60 +61,74 @@ def read_file(profile, startTime=10, endTime=-1, maxLayers=500, cut=0,
     z = -fz * tot / 2
     maxWidth = 0
     maxHeight = 0
-    
+    minX = 1e10
+    minY = 1e10
+
+    scut, ecut = None, None
+    if cut > 0:
+        scut, ecut = cut, -cut
     # Color calculation
     delta = tot
     minT = 0
     maxT = 0
     if T is not False:
-        mi, mt, minT = T.outfile.min(T.path, 
-                                     start_time=startTime, 
+        mi, mt, minT = T.outfile.min(T.path,
+                                     start_time=startTime,
                                      end_time=endTime)
-        Mi, Mt, maxT = T.outfile.max(T.path, 
-                                     start_time=startTime, 
+        Mi, Mt, maxT = T.outfile.max(T.path,
+                                     start_time=startTime,
                                      end_time=endTime)
-        delta = maxT-minT
-        
+        delta = maxT - minT
+
     def get_color(c, t):
         if T is False:
             pos = 1. * c / delta
         else:
             mi = T.get_time(t)
-            pos = (T[mi][1]-minT)/delta
+            pos = (T[mi][1] - minT) / delta
             print pos, T[mi][1], minT, delta
         col = list(clrmp.map(pos))
         # Adjust transparency to reduce bightness of saturated colors
         col[-1] = int(255 - (col[0] + col[1] + col[2]) / 5)
         return col
-        
+
     def abort():
         aborted[0] = True
-        
-    jobs((end-start), 'Extrusion', abort=abort)
+
+    jobs((end - start), 'Extrusion', abort=abort)
     for i in range(start, end, step):
         if aborted[0]:
             return False, False, False, False
-        job(i-start, 'Extrusion')
+        job(i - start, 'Extrusion')
         t, ((w, h), x, y) = profile[i]
-        x = x.astype(np.float32)
+        x = x.astype(np.float32)-x[0]
         y = y.astype(np.float32)
-        if cut:
-            x = x[cut:-cut]
-            y = y[cut:-cut]
-        x -= x.mean()
-        mW = max(x) - min(x)
+        if cut or deplete:
+            x = x[scut:ecut:deplete]
+            y = y[scut:ecut:deplete]
+        _minx = min(x)
+        if _minx<minX:
+            minX=_minx
+        mW = max(x) - _minx
         if mW > maxWidth:
             maxWidth = mW
-        mH = max(y) - min(y)
+        _miny = min(y)
+        if _miny<minY:
+            minY=_miny
+        mH = max(y) - _miny
         if mH > maxHeight:
             maxHeight = mH
         xs.append(x)
-        ys.append(-y)
-        zs.append(-z)
-        colors.append(get_color(c,t))
+        ys.append(y)
+        zs.append(z)
+        colors.append(get_color(c, t))
         # Should be calc depending on shot height
         z += fz
         c += 1
+    
+    for i,x in enumerate(xs):
+        x -=minX+maxWidth/2
+        ys[i]-=minY+maxHeight/2
     return xs, ys, zs, colors, minT, maxT
 
 
@@ -196,26 +210,26 @@ def surface3d(xs, ys, zs, colors, start=0, end=-1, step=1):
     # w.pan(0,0,0)
     return w
 
-def plot3d(xs, ys, zs, colors, start=0, end=-1, step=1,
-                 aborted=[False],
-                 jobs=lambda *x, **k: 0,
-                 job=lambda *x, **k: 0,
-                 done=lambda *x, **k: 0):
-    
+
+def plot3d(xs, ys, zs, colors,
+           aborted=[False],
+           jobs=lambda *x, **k: 0,
+           job=lambda *x, **k: 0,
+           done=lambda *x, **k: 0):
+
     w = gl.GLViewWidget()
+
     def abort():
-        aborted[0]=True
+        aborted[0] = True
     jobs(len(xs), 'Extrusion', abort=abort)
-    
+
     for i, x in enumerate(xs):
         if aborted[0]:
             logging.debug('Estrusion aborted')
             return False
         job(i, 'Extrusion')
-        sampled_x = x[start:end:step]
-        sampled_y = ys[i][start:end:step] - ys[i][0]
-        z = np.ones(len(sampled_x)) * zs[i]
-        plt = plot_line(sampled_x, sampled_y, z, color=colors[i])
+        z = np.ones(len(x)) * zs[i]
+        plt = plot_line(x, -ys[i], -z, color=colors[i])
         plt.scale(0.005, 0.005, 0.005)
         w.addItem(plt)
 
@@ -226,38 +240,34 @@ def plot3d(xs, ys, zs, colors, start=0, end=-1, step=1,
     return w
 
 
-def extrude(f, data_path, config={'cut':0}):
+def extrude(f, data_path, config={'cut': 0}):
     data_path = data_path.split(':')[-1]
     if not data_path.startswith('/'):
         data_path = '/' + data_path
     prf = get_node_reference(f, data_path)
-    T = '/'.join(data_path.split('/')[:-1])+'/T'
+    T = '/'.join(data_path.split('/')[:-1]) + '/T'
     T = get_node_reference(f, T)
     xs, ys, zs, colors, minT, maxT = read_file(prf, T=T, **config)
     w = plot3d(xs, ys, zs, colors)
     return w
 
-def deferred_extrusion(f, dataPath, config={}, aborted=[False],
-                 jobs=lambda *x, **k: 0,
-                 job=lambda *x, **k: 0,
-                 done=lambda *x, **k: 0):
+
+def deferred_extrusion(prf, T, config={}, aborted=[False],
+                       jobs=lambda *x, **k: 0,
+                       job=lambda *x, **k: 0,
+                       done=lambda *x, **k: 0):
     """Run data collection in a separate thread and emit signal when GL widget is ready"""
 
-    
-    data_path = dataPath.split(':')[-1]
-    if not data_path.startswith('/'):
-        data_path = '/' + data_path
-    prf = get_node_reference(f, data_path)
-    T = '/'.join(data_path.split('/')[:-1])+'/T'
-    T = get_node_reference(f, T)
     def abort():
-        aborted[0]=True
+        aborted[0] = True
 
-    thread = widgets.RunMethod(read_file, prf, T=T, aborted=aborted, jobs=jobs, job=job, done=done, **config)
-    
+    thread = widgets.RunMethod(
+        read_file, prf, T=T, aborted=aborted, jobs=jobs, job=job, done=done, **config)
+
     thread.pid = 'Extrusion'
     thread.abort = abort
     thread.emit_result = False
+
     def process_result():
         if thread.result is None:
             logging.error('No Extrusion result!')
@@ -269,71 +279,180 @@ def deferred_extrusion(f, dataPath, config={}, aborted=[False],
         w = plot3d(xs, ys, zs, colors)
         thread.widget = w
         thread.notifier.emit(QtCore.SIGNAL('widget_ready()'))
-        
-    thread.notifier.connect(thread.notifier, QtCore.SIGNAL('done()'), process_result)
-    QtCore.QThreadPool.globalInstance().start(thread)    
-    
+
+    thread.notifier.connect(
+        thread.notifier, QtCore.SIGNAL('done()'), process_result)
+    QtCore.QThreadPool.globalInstance().start(thread)
+
     return thread
+
 
 opts = {}
 ao(opts, 'dataPath', 'String', '', _('Profile source'))
-ao(opts, 'startTime', 'Float', 0, _('Start time'))
-ao(opts, 'endTime', 'Float', -1, _('End time'), min=-1)
+ao(opts, 'startTime', 'Float', 0, _('Start time'), min=0, max=36000, unit='second')
+ao(opts, 'endTime', 'Float', -1, _('End time'), min=-1, max=36000, unit='second')
 ao(opts, 'maxLayers', 'Integer', 500, _('Max layers'), max=2000, min=10)
 ao(opts, 'cut', 'Integer', 0, _('Remove points from start/end'), min=0)
+ao(opts, 'reset', 'Boolean', False, _('Reset scene'))
 
-#TODO: add startTemp, endTemp and update one/another as a function
 
+class ColorMapLegend(QtGui.QWidget):
+    levels = 10
+
+    def __init__(self, low=0, high=1, parent=None):
+        super(ColorMapLegend, self).__init__(parent=parent)
+        self.labels = []
+        self.colors = []
+        self.lay = QtGui.QVBoxLayout()
+        self.setLayout(self.lay)
+        self.clrmp = clrmp
+        self.low = low
+        self.high = high
+        self.delta = high - low
+
+    def set_range(self, low, high):
+        self.low = low
+        self.high = high
+        self.delta = high - low
+        d = self.delta / (self.levels - 1)
+
+        for i in range(self.levels):
+            if i > len(self.labels) - 1:
+                lbl = QtGui.QLabel()
+                self.labels.append(lbl)
+                self.lay.addWidget(lbl)
+            lbl = self.labels[i]
+            cur = low + d * i
+            lbl.setText('{:.0f}'.format(cur))
+            f0 = 1. * i / self.levels
+            c = self.clrmp.mapToQColor(f0)
+            c1 = 'white' if f0 < 0.5 else 'black'
+
+            lbl.setStyleSheet(
+                "background-color: {}; color: {};".format(c.name(), c1))
+
+        # Remove excess labels if any
+        for i in range(self.levels - len(self.labels)):
+            lbl = self.labels[i + self.levels]
+            lbl.hide()
+            lbl.deleteLater()
+
+
+# TODO: add startTemp, endTemp and update one/another as a function
+# TODO: display colormap legend
 class ExtrusionRender(QtGui.QWidget):
     render_thread = False
     render_widget = False
+
     def __init__(self, shared_file, data_path, parent=None):
         super(ExtrusionRender, self).__init__(parent=parent)
+        self.setWindowTitle(_('3D Sample Profile Extrusion'))
         self.shared_file = shared_file
         self.lay = QtGui.QHBoxLayout()
-        self.setLayout(self.lay )
+        self.setLayout(self.lay)
         ropts = deepcopy(opts)
         ropts['dataPath']['current'] = data_path
         self.cfg = ConfigurationProxy({'self': ropts})
+        self.cfg.callbacks_set = set([self.set_limits])
         self.wg = conf.Interface(self.cfg, self.cfg, opts)
         self.labelT = QtGui.QLabel()
         self.btn_ok = QtGui.QPushButton(_('Render'))
-        self.wg.sectionsMap['Main'].layout().addWidget(self.labelT)
-        self.wg.sectionsMap['Main'].layout().addWidget(self.btn_ok)
-        self.wg.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        self.legend = ColorMapLegend()
+        self.left_lay = self.wg.sectionsMap['Main'].layout()
+        self.left_lay.addWidget(self.labelT)
+        self.left_lay.addWidget(self.legend)
+        self.left_lay.layout().addWidget(self.btn_ok)
+        self.left_lay.layout().addStretch()
+        self.wg.setSizePolicy(QtGui.QSizePolicy.Minimum,
+                              QtGui.QSizePolicy.Preferred)
         self.wg.setMaximumWidth(300)
         self.lay.addWidget(self.wg)
         self.btn_ok.clicked.connect(self.refresh)
-        
-    def refresh(self):
-        config = self.cfg.asdict()
-        data_path = config.pop('dataPath')
-        self.render_thread = deferred_extrusion(self.shared_file, data_path, config)
-        self.render_thread.connect(self.render_thread.notifier, 
-                                   QtCore.SIGNAL('widget_ready()'), 
-                                   self.replace_widget)
+        self.set_limits()
+
+    def set_limits(self, cfg=None, key=None, old=None, new=None):
+        data_path = self.cfg['dataPath'].split(':')[-1]
+        if not data_path.startswith('/'):
+            data_path = '/' + data_path
+        self.ref_profile = get_node_reference(self.shared_file, data_path)
+        T = '/'.join(data_path.split('/')[:-1]) + '/T'
+        self.ref_T = get_node_reference(self.shared_file, T)
+
+        st = self.cfg['startTime']
+        et = self.cfg['endTime']
+        if key == 'endTime':
+            et = new
+        elif key == 'startTime':
+            st = new
+        mintime = self.ref_profile[0][0]
+        maxtime = self.ref_profile[-1][0]
+        self.cfg.setattr('startTime', 'min', mintime)
+        self.cfg.setattr('startTime', 'max', et if et > 0 else maxtime)
+        self.wg.widgetsMap['startTime'].update_option()
+        self.wg.widgetsMap['startTime'].changed_option()
+        self.cfg.setattr('endTime', 'max', maxtime)
+        self.wg.widgetsMap['endTime'].update_option()
+        self.wg.widgetsMap['endTime'].changed_option()
+
+        i = 0
+        if st > 0:
+            i = self.ref_T.get_time(st)
+        t0, T0 = self.ref_T[i]
+        i = -1
+        if et > st:
+            i = self.ref_T.get_time(et)
+        t1, T1 = self.ref_T[i]
+        self.labelT.setText(
+            u'Temperature range:\n {:.1f}°C - {:.1f}°C'.format(T0, T1))
+        return new
     
+    refreshing = False
+    def refresh(self):
+        if self.refreshing:
+            return False
+        self.refreshing = True
+        config = self.cfg.asdict()
+        config.pop('dataPath')
+        config.pop('reset')
+        self.render_thread = deferred_extrusion(
+            self.ref_profile, self.ref_T, config)
+        self.render_thread.connect(self.render_thread.notifier,
+                                   QtCore.SIGNAL('widget_ready()'),
+                                   self.replace_widget)
+        self.set_limits()
+
     def replace_widget(self):
-        if self.render_widget:
+        opts = {}
+        if self.render_widget and self.render_widget.isVisible():
+            opts = self.render_widget.opts
             self.render_widget.hide()
             self.render_widget.deleteLater()
         self.render_widget = self.render_thread.widget
-        #self.render_widget.show()
+        # self.render_widget.show()
         self.render_widget.setMinimumWidth(800)
         self.render_widget.setMinimumHeight(600)
-        self.render_widget.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
+        self.render_widget.setSizePolicy(
+            QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
         self.lay.addWidget(self.render_widget)
         minT, maxT = self.render_thread.result[-2:]
-        self.labelT.setText(u'Temperature range:\n {:.1f}°C - {:.1f}°C'.format(minT, maxT))
+        self.labelT.setText(
+            u'Temperature range:\n {:.1f}°C - {:.1f}°C'.format(minT, maxT))
+        self.legend.set_range(minT, maxT)
+        if opts and not self.cfg['reset']:
+            self.render_widget.setCameraPosition(distance=opts['distance'],
+                                                 elevation=opts['elevation'],
+                                                 azimuth=opts['azimuth'])
+            c = opts['center']
+            self.render_widget.pan(c.x(), c.y(), c.z(), relative=False)
+        
+        self.refreshing = False
 
 
 if __name__ == '__main__':
     test_path = '/home/daniele/MisuraData/hsm/BORAX powder 10 C min.h5'
     data_path = '/hsm/sample0/profile'
-    cut = 200
-    test_path = '/home/daniele/MisuraData/horizontal/profiles/System Interbau 80 1400.h5'
-    data_path = '/horizontal/sample0/Right/profile'
-    cut = 0
+    #test_path = '/home/daniele/MisuraData/horizontal/profiles/System Interbau 80 1400.h5'
+    #data_path = '/horizontal/sample0/Right/profile'
     app = pg.QtGui.QApplication([])
     from misura.canon.indexer import SharedFile
     f = SharedFile(test_path)
