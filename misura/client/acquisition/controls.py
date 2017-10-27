654474131ba45ba9726c6f67e9485f172c6b8f54
Daniele Paganelli <dpaganelli@tainstruments.com>
2017-10-27 09:57:53 +0200

    Replaced Focus control with a proper MotorSlider

--- misura/client/acquisition/controls.py
+++ misura/client/acquisition/controls.py
@@ -315,8 +315,6 @@ class MotionControls(QtGui.QToolBar):
             self.add_focus(obj)
 
     def add_focus(self, obj):
-        #       slider=widgets.MotorSlider(self.server,obj,self.parent())
-        slider = widgets.build(self.server, obj, obj.gete('goingTo'))
-        slider.lay.insertWidget(0, slider.label_widget)
-        slider.label_widget.setText('    Focus:')
+        slider=widgets.MotorSlider(self.server,obj,self.parent())
+        slider.lay.insertWidget(0, QtGui.QLabel(_('    Focus:')))
         self.addWidget(slider)
