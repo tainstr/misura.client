16c4cd4e545a73f5f9c9beb47c4837fcc4768965
Daniele Paganelli <dpaganelli@tainstruments.com>
2018-11-28 01:22:12 +0100

    OptionLabel resolves RoleIO, FLTD-572

--- misura/client/plugin/OptionLabel.py
+++ misura/client/plugin/OptionLabel.py
@@ -113,9 +113,17 @@ class OptionLabel(utils.OperationWrapper, widgets.TextLabel):
         opt = proxy.gete(opt_name)
         if 'Hidden' in opt['attr']:
             return None
+        typ = opt['type']
+        # Resolve RoleIO pointers
+        while typ=='RoleIO':
+            proxy, opt_name = proxy.from_column(opt['options'][0])
+            if not proxy or not opt_name or opt_name not in proxy:
+                logging.info('Cannot resolve RoleIO', opt_name, opt['options'])
+                return None
+            opt = proxy.gete(opt_name)
+            typ = opt['type']
         fmt = self.settings.format
         val = proxy[opt_name]
-        typ = opt['type']
         name = ''
         if self.settings.showName:
             name = opt['name']+': '
